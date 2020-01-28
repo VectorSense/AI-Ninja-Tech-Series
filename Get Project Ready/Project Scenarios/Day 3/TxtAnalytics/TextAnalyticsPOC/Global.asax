88bfbfc8c26be3b3ba40de3950909a83f82bb916
Vignesh-JSN <vignesh@jumpstartninja.com>
2020-01-29 01:21:12 +0530

    Updated

---
+++ Get Project Ready/Project Scenarios/Day 3/TxtAnalytics/TextAnalyticsPOC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TextAnalyticsPOC.MvcApplication" Language="C#" %>
---
+++ Get Project Ready/Project Scenarios/Day 3/TxtAnalytics/TextAnalyticsPOC/LanguageDetectionSample.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.Azure.CognitiveServices.Language.TextAnalytics.Models;
+using Microsoft.Azure.CognitiveServices.Language.TextAnalytics;
+
+namespace PartnerTechSeries
+{
+    namespace AI
+    {
+        namespace Demo
+        {
+            namespace TextAnalytics
+            {
+                public class LanguageDetectionSample
+                {
+                    public List<string> Language = new List<string>();
+                    public async Task RunAsync(string endpoint, string key, string text)
+                    {
+                        var credentials = new ApiKeyServiceClientCredentials(key);
+                        var client = new TextAnalyticsClient(credentials)
+                        {
+                            Endpoint = endpoint
+                        };
+
+                        // The documents to be submitted for language detection. The ID can be any value.
+                        var inputDocuments = new LanguageBatchInput(
+                                new List<LanguageInput>
+                                    {
+                                        new LanguageInput(id: "1", text: text)
+                                    });
+
+                        var langResults = await client.DetectLanguageAsync(false, inputDocuments);
+
+                        // Printing detected languages
+                          foreach (var document in langResults.Documents)
+                        {
+                            Language.Add($"{document.DetectedLanguages[0].Name}");
+                            //Console.WriteLine($"Document ID: {document.Id} , Language: {document.DetectedLanguages[0].Name}");
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
