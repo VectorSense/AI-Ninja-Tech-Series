ac3294070f79e9ecd4229240b14587001e71dc4b
Vignesh-JSN <vignesh@jumpstartninja.com>
2020-01-29 00:11:00 +0530

    Updated

---
+++ Get Project Ready/Challenges/ChatBot/ProjectManagementBot/packages/Microsoft.Bot.Builder.3.15.2.2/src/Microsoft.Bot.Connector.AspNetCore2/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyTitle("Microsoft.Bot.Connector.AspNetCore")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Microsoft Corporation")]
+[assembly: AssemblyProduct("Microsoft Bot Framework")]
+[assembly: AssemblyCopyright("Copyright © Microsoft Corporation 2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+
+[assembly: AssemblyVersion("2.0.1.7")]
+[assembly: AssemblyFileVersion("2.0.1.7")]
+
+//[assembly: AssemblyKeyFileAttribute(@"..\\..\\buildtools\\35MSSharedLib1024.snk")] 
+//[assembly: AssemblyDelaySignAttribute(true)]
+
+[assembly: AssemblyKeyFileAttribute(@"..\\..\\buildtools\\35MSSharedLib1024.snk")] 
+ [assembly: AssemblyDelaySignAttribute(true)]
